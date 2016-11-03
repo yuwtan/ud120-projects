48c930dc132953a5c645888f560e87ae22616804
Yuwen Tan <yuwtan@gmail.com>
2016-11-02 21:54:38 -0500

    Finish data and questions

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,16 +19,46 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
-print(len(enron_data))
+print len(enron_data)
 
 poi = 0
 for key in enron_data:
     if enron_data[key]["poi"] == 1:
         poi += 1
-print(poi)
+print poi
 
-print(enron_data["PRENTICE JAMES"]["total_stock_value"])
+print enron_data["PRENTICE JAMES"]["total_stock_value"]
 
-print(enron_data["COLWELL WESLEY"]["from_this_person_to_poi"])
+print enron_data["COLWELL WESLEY"]["from_this_person_to_poi"]
 
-print(enron_data["SKILLING JEFFREY K"]["exercised_stock_options"])
+print enron_data["SKILLING JEFFREY K"]["exercised_stock_options"]
+
+print enron_data["LAY KENNETH L"]["total_payments"]
+
+salary = 0
+email_address = 0
+for key in enron_data:
+    if enron_data[key]["salary"] != "NaN":
+        salary += 1
+    if enron_data[key]["email_address"] != "NaN":
+        email_address += 1
+print salary
+print email_address
+
+total_payments = 0
+for key in enron_data:
+    if enron_data[key]["total_payments"] == "NaN":
+        total_payments += 1
+print total_payments
+print float(total_payments)/len(enron_data)
+
+
+total_payments_poi = 0
+for key in enron_data:
+    if enron_data[key]["total_payments"] == "NaN" \
+            and enron_data[key]["poi"] == 1:
+        total_payments_poi += 1
+print float(total_payments_poi)/poi
+
+for s in enron_data["LAY KENNETH L"]:
+    print s, enron_data["LAY KENNETH L"][s]
