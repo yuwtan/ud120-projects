7a29915f1ca5030823bd715ac31cb3a93bed56e2
Yuwen Tan <yuwtan@gmail.com>
2016-11-09 21:59:11 -0600

    save feature selection for first time

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -8,11 +8,10 @@ numpy.random.seed(42)
 ### The words (features) and authors (labels), already largely processed.
 ### These files should have been created from the previous (Lesson 10)
 ### mini-project.
-words_file = "../text_learning/your_word_data.pkl" 
-authors_file = "../text_learning/your_email_authors.pkl"
+words_file = "./word_data_overfit.pkl"
+authors_file = "./email_authors_overfit.pkl"
 word_data = pickle.load( open(words_file, "r"))
 authors = pickle.load( open(authors_file, "r") )
-
 
 
 ### test_size is the percentage of events assigned to the test set (the
@@ -38,6 +37,16 @@ labels_train   = labels_train[:150]
 
 
 ### your code goes here
+from sklearn import tree
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features_train, labels_train)
 
+print len(features_train)
 
+print clf.score(features_test, labels_test)
 
+for i in range(features_train.shape[1]):
+    if clf.feature_importances_[i] >= 0.2:
+        print vectorizer[i]
+        print vectorizer.get_feature_names()[i]
+        print clf.feature_importances_[i]
